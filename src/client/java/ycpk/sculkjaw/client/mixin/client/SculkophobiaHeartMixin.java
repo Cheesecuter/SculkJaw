bb2045cfe774f46324412aa07c8ef07bf32ffe76
Cheesecuter <Cheesecuter777@gmail.com>
2026-01-06 22:51:49 +0800

    1.0.2 Sculkophobia Heart now have shaking and blinking visual effect

--- src/client/java/ycpk/sculkjaw/client/mixin/client/SculkophobiaHeartMixin.java
+++ src/client/java/ycpk/sculkjaw/client/mixin/client/SculkophobiaHeartMixin.java
@@ -7,8 +7,12 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -20,8 +24,25 @@ import ycpk.sculkjaw.Sculkjaw;
 @Environment(EnvType.CLIENT)
 @Mixin(Gui.class)
 public class SculkophobiaHeartMixin {
+    @Shadow @Final private RandomSource random;
     @Unique
     float sculkophobiaHearts = 0.0F;
+    @Unique
+    private static final ResourceLocation fullHeart = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/sculkophobia_full.png");
+    @Unique
+    private static final ResourceLocation fullHeartBlinking = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/sculkophobia_full_blinking.png");
+    @Unique
+    private static final ResourceLocation fullHeartHardcore = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/sculkophobia_hardcore_full.png");
+    @Unique
+    private static final ResourceLocation fullHeartHardcoreBlinking = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/sculkophobia_hardcore_full_blinking.png");
+    @Unique
+    private static final ResourceLocation container = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/container.png");
+    @Unique
+    private static final ResourceLocation containerBlinking = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/container_blinking.png");
+    @Unique
+    private static final ResourceLocation containerHardcore = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/container_hardcore.png");
+    @Unique
+    private static final ResourceLocation containerHardcoreBlinking = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/container_hardcore_blinking.png");
 
     public SculkophobiaHeartMixin() {
 
@@ -31,7 +52,7 @@ public class SculkophobiaHeartMixin {
     private void renderSculkophobiaHearts(GuiGraphics guiGraphics, Player player,
                                           int x, int y, int lines, int regeneratingHeartIndex,
                                           float maxHealth, int lastHealth, int health,
-                                          int absorption, boolean blinking, CallbackInfo ci) {
+                                          int absorption, boolean half, CallbackInfo ci) {
         if(!(player instanceof LocalPlayer)) {
             return;
         }
@@ -39,18 +60,38 @@ public class SculkophobiaHeartMixin {
         if(sculkophobiaHearts <= 0) {
             return;
         }
-        ResourceLocation fullHeart = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/sculkophobia_full.png");
-        ResourceLocation hardcoreFullHeart = ResourceLocation.fromNamespaceAndPath(Sculkjaw.MOD_ID, "textures/gui/sprites/hud/heart/sculkophobia_hardcore_full.png");
+        int p = Mth.ceil((double) maxHealth / 2.0);
+        int q = Mth.ceil((double) absorption / 2.0);
+        int s = p + q - 1;
+        int s2 = s * 2;
+        boolean bl = (s2 + 1) == lastHealth;
+
         for(int i = 0; i < this.sculkophobiaHearts; ++i) {
             int row = i / 10;
             int column = i % 10;
             int x2 = x + (9 - column) * 8;
             int y2 = y - row * 10;
+            x2 += this.random.nextInt(2);
+            y2 += this.random.nextInt(2);
             if(player.level().getLevelData().isHardcore()) {
-                guiGraphics.blit(RenderPipelines.GUI_TEXTURED, hardcoreFullHeart, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                if(bl) {
+                    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, containerHardcoreBlinking, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, fullHeartHardcoreBlinking, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                }
+                else {
+                    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, containerHardcore, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, fullHeartHardcore, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                }
             }
             else {
-                guiGraphics.blit(RenderPipelines.GUI_TEXTURED, fullHeart, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                if(bl) {
+                    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, containerBlinking, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, fullHeartBlinking, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                }
+                else {
+                    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, container, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, fullHeart, x2, y2, 0.0F, 0.0F, 9, 9, 9, 9);
+                }
             }
         }
     }
