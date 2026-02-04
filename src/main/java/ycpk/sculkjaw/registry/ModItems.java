a558fe9f7c9a7ee15ebbfda6a9fe884e7af9dae4
Cheesecuter <Cheesecuter777@gmail.com>
2026-02-05 04:33:03 +0800

    1.0.3 Added mob custom creative tab

--- src/main/java/ycpk/sculkjaw/registry/ModItems.java
+++ src/main/java/ycpk/sculkjaw/registry/ModItems.java
@@ -1,22 +1,14 @@
 package ycpk.sculkjaw.registry;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BucketItem;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import ycpk.sculkjaw.Sculkjaw;
 import ycpk.sculkjaw.level.material.ModFluids;
 
 public class ModItems {
     public static void registerModItems(){
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.NATURAL_BLOCKS).register(ModItems::addItemsToNatureBlocksTabItemGroup);
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(ModItems::addItemsToToolsAndUtilitiesTabItemGroup);
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(ModItems::addItemsToFoodAndDrinksTabItemGroup);
         Sculkjaw.LOGGER.info("Registering Items for Mod " + Sculkjaw.MOD_ID);
     }
 
@@ -30,16 +22,4 @@ public class ModItems {
     public static final Item ACIDOPHILIC_CORDYCEPS = Items.registerBlock(ModBlocks.ACIDOPHILIC_CORDYCEPS);
     public static final Item UMBRAFERN = Items.registerBlock(ModBlocks.UMBRAFERN);
     public static final Item LARGE_UMBRAFERN = Items.registerBlock(ModBlocks.LARGE_UMBRAFERN);
-
-    private static void addItemsToNatureBlocksTabItemGroup(FabricItemGroupEntries entries) {
-        entries.addAfter(Items.SCULK_SENSOR, SCULK_JAW);
-        entries.addAfter(ModItems.SCULK_JAW, CONCENTRATED_SCULK, ACIDOPHILIC_CORDYCEPS, UMBRAFERN, LARGE_UMBRAFERN);
-    }
-
-    private static void addItemsToToolsAndUtilitiesTabItemGroup(FabricItemGroupEntries entries) {
-        entries.addAfter(Items.MILK_BUCKET, SCULK_ACID_BUCKET);
-    }
-
-    private static void addItemsToFoodAndDrinksTabItemGroup(FabricItemGroupEntries entries) {
-    }
 }
