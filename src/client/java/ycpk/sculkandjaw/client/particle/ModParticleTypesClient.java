2e61c04408641966d94e1b8067b039f5ee1e9624
Cheesecuter <Cheesecuter777@gmail.com>
2026-06-22 23:10:10 +0800

    Added Sculk Acid Dripping particle

--- src/client/java/ycpk/sculkandjaw/client/particle/ModParticleTypesClient.java
+++ src/client/java/ycpk/sculkandjaw/client/particle/ModParticleTypesClient.java
@@ -13,6 +13,9 @@ public class ModParticleTypesClient {
         SculkAndJaw.LOGGER.info("Registering Client Particle Types for Mod " + SculkAndJaw.MOD_ID);
         ParticleFactoryRegistry.getInstance().register(ModParticleTypes.SCULKOPHOBIA, SpellParticle.Provider::new);
         ParticleFactoryRegistry.getInstance().register(ModParticleTypes.UMBRAFERN_SPORE, UmbrafernSporeParticle.Provider::new);
+        ParticleFactoryRegistry.getInstance().register(ModParticleTypes.DRIPPING_SCULK_ACID, DrippingSculkAcidParticle.SculkAcidHangProvider::new);
+        ParticleFactoryRegistry.getInstance().register(ModParticleTypes.FALLING_SCULK_ACID, DrippingSculkAcidParticle.SculkAcidFallProvider::new);
+        ParticleFactoryRegistry.getInstance().register(ModParticleTypes.LANDING_SCULK_ACID, DrippingSculkAcidParticle.SculkAcidLandProvider::new);
         ParticleFactoryRegistry.getInstance().register(ModParticleTypes.SCULK_ACID_BUBBLE_PARTICLE, SculkAcidBubbleParticle.Provider::new);
         ParticleFactoryRegistry.getInstance().register(ModParticleTypes.SCULK_ACID_BUBBLE_POP_PARTICLE, SculkAcidBubblePopParticle.Provider::new);
     }
