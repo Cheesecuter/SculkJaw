25e3cd3eec51ca4a489f49007ac31f2386dfd13c
Cheesecuter <Cheesecuter777@gmail.com>
2026-06-27 16:55:26 +0800

    Added mod damage types

---
+++ src/main/java/com/ycpk/sculkandjaw/registry/ModDamageTypes.java
@@ -0,0 +1,16 @@
+package com.ycpk.sculkandjaw.registry;
+
+import com.ycpk.sculkandjaw.SculkAndJaw;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.damagesource.DamageType;
+
+public class ModDamageTypes {
+    public static void registerModDamageTypes(){
+        SculkAndJaw.LOGGER.info("Registering Damage Types for Mod " + SculkAndJaw.MOD_ID);
+    }
+
+    public static ResourceKey<DamageType> SCULK_JAW_BITE = ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(SculkAndJaw.MOD_ID, "sculk_jaw_bite"));
+    public static ResourceKey<DamageType> SCULK_ACID = ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(SculkAndJaw.MOD_ID, "sculk_acid"));
+}
