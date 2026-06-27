d1af9640efc03b6f92716dc8b8c2e029a9638d87
Cheesecuter <Cheesecuter777@gmail.com>
2026-06-27 16:55:47 +0800

    Added mod tags

---
+++ src/main/java/com/ycpk/sculkandjaw/registry/ModTags.java
@@ -0,0 +1,9 @@
+package com.ycpk.sculkandjaw.registry;
+
+import com.ycpk.sculkandjaw.SculkAndJaw;
+
+public class ModTags {
+    public static void registerModTags(){
+        SculkAndJaw.LOGGER.info("Registering Tags for Mod " + SculkAndJaw.MOD_ID);
+    }
+}
