85ab9b535cd4e4a91a36ce0ed852576d322501aa
Cheesecuter <Cheesecuter777@gmail.com>
2026-06-29 01:42:51 +0800

    Added Sculk Jaw and Concentrated Sculk

---
+++ src/main/java/com/ycpk/sculkandjaw/blocks/blockentities/SculkJawBlockEntity.java
@@ -0,0 +1,249 @@
+package com.ycpk.sculkandjaw.blocks.blockentities;
+
+import com.ycpk.sculkandjaw.registry.ModBlockEntities;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.Container;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.RandomizableContainer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootTable;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class SculkJawBlockEntity extends BlockEntity implements RandomizableContainer {
+    private boolean aIsLargeEntity = false;
+    private boolean aIsBitingLargeEntity = false;
+    private boolean aIsDecomposingEntity = false;
+    private boolean aIsEffectingEntity = true;
+    private boolean aHasCombined = false;
+    private float aBiteDamage = 2.0F;
+    private float aAcidDamage = 1.0F;
+    private int aEffectAmplifier = 0;
+    private int aAcidCounter = 0;
+    private int aExperienceReward = 5;
+    public int tickCount = 0;
+    private Set<UUID> aBiteDamageEntities = null;
+    private Set<UUID> aAcidDamageEntities = null;
+    private Set<UUID> aSculkophobiaEffectEntities = null;
+    private NonNullList<ItemStack> aItems;
+
+    public SculkJawBlockEntity(BlockPos blockPos, BlockState blockState) {
+        super(ModBlockEntities.SCULK_JAW_BLOCK_ENTITY.get(), blockPos, blockState);
+        this.aBiteDamageEntities = new HashSet<>();
+        this.aAcidDamageEntities = new HashSet<>();
+        this.aSculkophobiaEffectEntities = new HashSet<>();
+        this.aItems = NonNullList.withSize(27, ItemStack.EMPTY);
+    }
+
+    @Override
+    public void setLootTable(@Nullable ResourceKey<LootTable> resourceKey) {
+
+    }
+
+    @Override
+    public @Nullable ResourceKey<LootTable> getLootTable() {
+        return null;
+    }
+
+    @Override
+    public void setLootTableSeed(long l) {
+
+    }
+
+    @Override
+    public long getLootTableSeed() {
+        return 0;
+    }
+
+    @Override
+    public int getContainerSize() {
+        return this.aItems.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void setItem(int i, ItemStack itemStack) {
+        this.unpackLootTable((Player) null);
+        this.getItems().set(i, itemStack);
+        itemStack.limitSize(this.getMaxStackSize(itemStack));
+    }
+
+    @Override
+    public ItemStack getItem(int i) {
+        return (ItemStack) this.aItems.get(i);
+    }
+
+    @Override
+    public ItemStack removeItem(int i, int j) {
+        this.unpackLootTable((Player) null);
+        return ContainerHelper.removeItem(this.getItems(), i, j);
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int i) {
+        this.unpackLootTable((Player)null);
+        return ContainerHelper.takeItem(this.getItems(), i);
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return Container.stillValidBlockEntity(this, player);
+    }
+
+    @Override
+    public void clearContent() {
+
+    }
+
+    public void setIsLargeEntity(boolean bl) {this.aIsLargeEntity = bl;}
+
+    public boolean getIsLargeEntity() {return this.aIsLargeEntity;}
+
+    public void setIsBitingLargeEntity(boolean bl) {this.aIsBitingLargeEntity = bl;}
+
+    public boolean getIsBitingLargeEntity() {return this.aIsBitingLargeEntity;}
+
+    public void setIsDecomposingEntity(boolean bl) {this.aIsDecomposingEntity = bl;}
+
+    public boolean getIsDecomposingEntity() {return this.aIsDecomposingEntity;}
+
+    public void setIsEffectingEntity(boolean bl) {this.aIsEffectingEntity = bl;}
+
+    public boolean getIsEffectingEntity() {return this.aIsEffectingEntity;}
+
+    public void setBiteDamage(float f) {this.aBiteDamage = f;}
+
+    public float getBiteDamage() {return this.aBiteDamage;}
+
+    public void setAcidDamage(float f) {this.aAcidDamage = f;}
+
+    public float getAcidDamage() {return this.aAcidDamage;}
+
+    public void setEffectAmplifier(int i) {this.aEffectAmplifier = i;}
+
+    public int getEffectAmplifier() {return this.aEffectAmplifier;}
+
+    public void setAcidCounter(int i) {this.aAcidCounter = i;}
+
+    public int getAcidCounter() {return this.aAcidCounter;}
+
+    public void setHasCombined(boolean bl) {this.aHasCombined = bl;}
+
+    public boolean getHasCombined() {return this.aHasCombined;}
+
+    public void addBiteDamageEntity(UUID uuid) {this.aBiteDamageEntities.add(uuid);}
+
+    public Set<UUID> getBiteDamageEntities() {return this.aBiteDamageEntities;}
+
+    public void removeBiteDamageEntity(UUID uuid) {this.aBiteDamageEntities.remove(uuid);}
+
+    public void addAcidDamageEntity(UUID uuid) {this.aAcidDamageEntities.add(uuid);}
+
+    public Set<UUID> getAcidDamageEntities() {return this.aAcidDamageEntities;}
+
+    public void removeAcidDamageEntity(UUID uuid) {this.aAcidDamageEntities.remove(uuid);}
+
+    public void addSculkophobiaEffectEntity(UUID uuid) {this.aSculkophobiaEffectEntities.add(uuid);}
+
+    public Set<UUID> getSculkophobiaEffectEntities() {return this.aSculkophobiaEffectEntities;}
+
+    public void removeSculkophobiaEffectEntity(UUID uuid) {this.aSculkophobiaEffectEntities.remove(uuid);}
+
+    public void setExperienceReward(int i) {this.aExperienceReward = i;}
+
+    public void addExperienceReward(int i) {this.aExperienceReward += i;}
+
+    public int getExperienceReward() {return this.aExperienceReward;}
+
+    public void setItems(NonNullList<ItemStack> nonNullList) {this.aItems = nonNullList;}
+
+    public NonNullList<ItemStack> getItems() {return this.aItems;}
+
+    private static boolean canMergeItems(ItemStack itemStack, ItemStack itemStack2) {
+        return itemStack.getCount() <= itemStack.getMaxStackSize() && ItemStack.isSameItemSameComponents(itemStack, itemStack2);
+    }
+
+    public boolean addItem(ItemStack itemStack) {
+        for (int i = 0; i < this.aItems.size(); i++) {
+            ItemStack itemStack2 = this.aItems.get(i);
+            if (canMergeItems(itemStack2, itemStack)) {
+                int j = itemStack.getMaxStackSize() - itemStack2.getCount();
+                int k = Math.min(itemStack.getCount(), j);
+                itemStack.shrink(k);
+                itemStack2.grow(k);
+                if(itemStack.isEmpty()) {
+                    return true;
+                }
+            }
+            else if(itemStack2.isEmpty()) {
+                this.aItems.set(i, itemStack);
+                itemStack = ItemStack.EMPTY;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag compoundTag, HolderLookup.Provider provider) {
+        super.saveAdditional(compoundTag, provider);
+        compoundTag.putBoolean("aIsLargeEntity", aIsLargeEntity);
+        compoundTag.putBoolean("aIsDecomposingEntity", aIsDecomposingEntity);
+        compoundTag.putBoolean("aIsEffectingEntity", aIsEffectingEntity);
+        compoundTag.putBoolean("aHasCombined", aHasCombined);
+        compoundTag.putFloat("aBiteDamage", aBiteDamage);
+        compoundTag.putFloat("aAcidDamage", aAcidDamage);
+        compoundTag.putInt("aEffectAmplifier", aEffectAmplifier);
+        compoundTag.putInt("aAcidCounter", aAcidCounter);
+        compoundTag.putInt("aExperienceReward", aExperienceReward);
+        if(!this.trySaveLootTable(compoundTag)) {
+            ContainerHelper.saveAllItems(compoundTag, this.aItems, provider);
+        }
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag compoundTag, HolderLookup.Provider provider) {
+        super.loadAdditional(compoundTag, provider);
+        this.aIsLargeEntity = compoundTag.getBoolean("aIsLargeEntity");
+        this.aIsDecomposingEntity = compoundTag.getBoolean("aIsDecomposingEntity");
+        this.aIsEffectingEntity = compoundTag.getBoolean("aIsEffectingEntity");
+        this.aHasCombined = compoundTag.getBoolean("aHasCombined");
+        this.aBiteDamage = compoundTag.getFloat("aBiteDamage");
+        this.aAcidDamage = compoundTag.getFloat("aAcidDamage");
+        this.aEffectAmplifier = compoundTag.getInt("aEffectAmplifier");
+        this.aAcidCounter = compoundTag.getInt("aAcidCounter");
+        this.aExperienceReward = compoundTag.getInt("aExperienceReward");
+        this.aItems = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        if(!this.tryLoadLootTable(compoundTag)) {
+            ContainerHelper.loadAllItems(compoundTag, this.aItems, provider);
+        }
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider registryLookup) {
+        return saveWithoutMetadata(registryLookup);
+    }
+
+    public static void clientTick(Level level, BlockPos blockPos, BlockState blockState, SculkJawBlockEntity blockEntity) {
+        ++blockEntity.tickCount;
+    }
+
+    public static void serverTick(Level level, BlockPos blockPos, BlockState blockState, SculkJawBlockEntity blockEntity) {
+        ++blockEntity.tickCount;
+    }
+}
--- src/main/java/com/ycpk/sculkandjaw/blocks/modblocks/ConcentratedSculkBlock.java
+++ src/main/java/com/ycpk/sculkandjaw/blocks/modblocks/ConcentratedSculkBlock.java
@@ -1,4 +1,228 @@
 package com.ycpk.sculkandjaw.blocks.modblocks;
 
-public class ConcentratedSculkBlock {
+import com.mojang.serialization.MapCodec;
+import com.ycpk.sculkandjaw.blocks.blockentities.ConcentratedSculkEntity;
+import com.ycpk.sculkandjaw.registry.ModBlockEntities;
+import com.ycpk.sculkandjaw.registry.ModBlocks;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectListIterator;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+public class ConcentratedSculkBlock extends BaseEntityBlock implements SculkBehaviour {
+    public static final BooleanProperty COMBINED_WITH_SCULK_JAW = BooleanProperty.create("combined_with_sculk_jaw");
+    public static final BooleanProperty COMBINED_WITH_SCULK_CATALYST = BooleanProperty.create("combined_with_sculk_catalyst");
+    private int EXPERIENCE_REWARD = 5;
+    public static final VoxelShape COLLISION_SHAPE_NOT_COMBINED = Block.box(0, 0, 0, 16, 16, 16);
+    public static final VoxelShape COLLISION_SHAPE_COMBINED_OPEN = Shapes.or(
+            Block.box(0, 0, 0, 16, 32, 1),
+            Block.box(0, 0, 0, 1, 32, 16),
+            Block.box(0, 0, 15, 16, 32, 16),
+            Block.box(15, 0, 0, 16, 32, 16),
+            Block.box(0, 0, 0, 16, 1, 16)
+    );
+    public static final VoxelShape COLLISION_SHAPE_COMBINED_CLOSE = Shapes.or(
+            Block.box(0, 0, 0, 16, 32, 1),
+            Block.box(0, 0, 0, 1, 32, 16),
+            Block.box(0, 0, 15, 16, 32, 16),
+            Block.box(15, 0, 0, 16, 32, 16),
+            Block.box(0, 0, 0, 16, 1, 16),
+            Block.box(0, 31, 0, 16, 32, 16)
+    );
+
+    public ConcentratedSculkBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+        this.registerDefaultState(getStateDefinition().getPossibleStates().getFirst()
+                .setValue(COMBINED_WITH_SCULK_JAW, false)
+                .setValue(COMBINED_WITH_SCULK_CATALYST, false)
+        );
+    }
+
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return simpleCodec(ConcentratedSculkBlock::new);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return new ConcentratedSculkEntity(blockPos, blockState);
+    }
+
+    @Override
+    protected RenderShape getRenderShape(BlockState blockState) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return blockState.getValue(COMBINED_WITH_SCULK_JAW) ? COLLISION_SHAPE_COMBINED_CLOSE : COLLISION_SHAPE_NOT_COMBINED;
+    }
+
+    @Override
+    protected VoxelShape getInteractionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return COLLISION_SHAPE_NOT_COMBINED;
+    }
+
+    @Override
+    public VoxelShape getOcclusionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return Shapes.block();
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return blockState.getValue(COMBINED_WITH_SCULK_JAW) ? COLLISION_SHAPE_COMBINED_OPEN : COLLISION_SHAPE_NOT_COMBINED;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{COMBINED_WITH_SCULK_JAW, COMBINED_WITH_SCULK_CATALYST});
+    }
+
+    @Override
+    protected void spawnAfterBreak(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, ItemStack itemStack, boolean bl) {
+        super.spawnAfterBreak(blockState, serverLevel, blockPos, itemStack, bl);
+        /*if (bl) {
+            if(blockState.getValue(COMBINED_WITH_SCULK_JAW)) {
+                if(EnchantmentHelper.hasTag(itemStack, ModEnchantmentTags.COMBINED_SCULK_JAW_DROPPING)) {
+                    Direction direction = Direction.DOWN;
+                    LootTable lootTable = serverLevel.getServer().reloadableRegistries().getLootTable(ModBuiltInLootTables.SCULK_JAW_COMBINATION);
+                    LootParams lootParams = (new LootParams.Builder(serverLevel)).create(LootContextParamSets.EMPTY);
+                    ObjectArrayList<ItemStack> objectArrayList = lootTable.getRandomItems(lootParams);
+                    if (!objectArrayList.isEmpty()) {
+                        ObjectListIterator iterator = objectArrayList.iterator();
+                        while (iterator.hasNext()) {
+                            ItemStack itemStack1 = (ItemStack) iterator.next();
+                            ItemEntity itemEntity = new ItemEntity(serverLevel, (double) blockPos.getX() + 0.5 + (double) direction.getStepX() * 0.65, (double) blockPos.getY() + 0.1, (double) blockPos.getZ() + 0.5 + (double) direction.getStepZ() * 0.65, itemStack1);
+                            itemEntity.setDeltaMovement(0.05 * (double)direction.getStepX() + serverLevel.random.nextDouble() * 0.02, 0.05, 0.05 * (double)direction.getStepZ() + serverLevel.random.nextDouble() * 0.02);
+                            serverLevel.addFreshEntity(itemEntity);
+                        }
+                    }
+                }
+                else {
+                    serverLevel.getBlockEntity(blockPos,
+                            ModBlockEntities.CONCENTRATED_SCULK_ENTITY).ifPresent((concentratedSculkEntity -> {
+                        int experienceReward = concentratedSculkEntity.getExperienceReward() + EXPERIENCE_REWARD;
+                        this.tryDropExperience(serverLevel, blockPos, itemStack, ConstantInt.of(experienceReward));
+                    }));
+                }
+            }
+            else {
+                this.tryDropExperience(serverLevel, blockPos, itemStack, ConstantInt.of(EXPERIENCE_REWARD));
+            }
+        }*/
+    }
+
+    @Override
+    protected BlockState updateShape(BlockState blockState, Direction direction, BlockState blockState2, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos2) {
+        if(!blockState.canSurvive(levelAccessor, blockPos)) {
+            return Blocks.AIR.defaultBlockState();
+        }
+        else if(levelAccessor.getBlockState(blockPos.above()).getBlock().equals(ModBlocks.SCULK_JAW.get())) {
+            levelAccessor.getBlockEntity(blockPos.above(), ModBlockEntities.SCULK_JAW_BLOCK_ENTITY.get()).ifPresent((sculkJawBlockEntity -> {
+                if(!sculkJawBlockEntity.getHasCombined()) {
+                    sculkJawBlockEntity.setHasCombined(true);
+                    sculkJawBlockEntity.setBiteDamage(4.0F);
+                    sculkJawBlockEntity.setAcidDamage(15.0F);
+                    sculkJawBlockEntity.getLevel().addDestroyBlockEffect(blockPos.above(), blockState);
+                    sculkJawBlockEntity.getLevel().addDestroyBlockEffect(blockPos, blockState);
+                }
+            }));
+            levelAccessor.getBlockEntity(blockPos, ModBlockEntities.CONCENTRATED_SCULK_ENTITY.get()).ifPresent((concentratedSculkEntity -> {
+                if(!concentratedSculkEntity.getHasCombinedWithSculkJaw()) {
+                    concentratedSculkEntity.setHasCombinedWithSculkJaw(true);
+                }
+            }));
+            return blockState.setValue(COMBINED_WITH_SCULK_JAW, true);
+        }
+        else if(levelAccessor.getBlockState(blockPos.above()).getBlock().equals(Blocks.SCULK_CATALYST)) {
+            return blockState.setValue(COMBINED_WITH_SCULK_CATALYST, true);
+        }
+        else {
+            super.updateShape(blockState, direction, blockState2, levelAccessor, blockPos, blockPos2);
+        }
+        return blockState;
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
+        return isSculkJawDestroied(levelReader, blockPos, blockState);
+    }
+
+    @Override
+    public int attemptUseCharge(SculkSpreader.ChargeCursor chargeCursor, LevelAccessor levelAccessor, BlockPos blockPos, RandomSource randomSource, SculkSpreader sculkSpreader, boolean bl) {
+        int i = chargeCursor.getCharge();
+        if (i != 0 && randomSource.nextInt(sculkSpreader.chargeDecayRate()) == 0) {
+            BlockPos blockPos2 = chargeCursor.getPos();
+            boolean bl2 = blockPos2.closerThan(blockPos, (double)sculkSpreader.noGrowthRadius());
+            if (canPlaceGrowth(levelAccessor, blockPos2)) {
+                int j = sculkSpreader.growthSpawnCost();
+                if (randomSource.nextInt(j) < i) {
+                    BlockPos blockPos3 = blockPos2.above();
+                    BlockState blockState = Blocks.SCULK_CATALYST.defaultBlockState();
+                    levelAccessor.setBlock(blockPos3, blockState, 3);
+                    levelAccessor.playSound((Player) null, blockPos2, blockState.getSoundType().getPlaceSound(), SoundSource.BLOCKS, 1.0F, 1.0F);
+                }
+
+                return Math.max(0, i - j);
+            } else {
+                return randomSource.nextInt(sculkSpreader.additionalDecayRate()) != 0 ? i : i - (bl2 ? 1 : getDecayPenalty(sculkSpreader, blockPos2, blockPos, i));
+            }
+        } else {
+            return i;
+        }
+    }
+
+    private static boolean canPlaceGrowth(LevelAccessor levelAccessor, BlockPos blockPos) {
+        BlockState blockState = levelAccessor.getBlockState(blockPos.above());
+        if (blockState.isAir() || blockState.is(Blocks.WATER) && blockState.getFluidState().is(Fluids.WATER) || blockState.is(Blocks.SCULK_VEIN)) {
+            if(blockState.is(Blocks.SCULK_CATALYST)) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private static int getDecayPenalty(SculkSpreader sculkSpreader, BlockPos blockPos, BlockPos blockPos2, int i) {
+        int j = sculkSpreader.noGrowthRadius();
+        float f = Mth.square((float)Math.sqrt(blockPos.distSqr(blockPos2)) - (float)j);
+        int k = Mth.square(24 - j);
+        float g = Math.min(1.0F, f / (float)k);
+        return Math.max(1, (int)((float)i * g * 0.5F));
+    }
+
+    private static boolean isSculkJawDestroied(LevelReader levelReader, BlockPos blockPos, BlockState blockState) {
+        return levelReader.getBlockState(blockPos.above()).getBlock().equals(ModBlocks.SCULK_JAW.get()) && blockState.getValue(COMBINED_WITH_SCULK_JAW) || !blockState.getValue(COMBINED_WITH_SCULK_JAW);
+    }
 }
