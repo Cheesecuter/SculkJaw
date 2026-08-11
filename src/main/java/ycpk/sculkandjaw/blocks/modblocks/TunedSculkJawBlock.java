eecb5c6eb4d89e0a556731175ae61c4872140b39
Cheesecuter <Cheesecuter777@gmail.com>
2026-08-12 06:22:17 +0800

    Tuned Sculk Jaw now have all direction models

--- src/main/java/ycpk/sculkandjaw/blocks/modblocks/TunedSculkJawBlock.java
+++ src/main/java/ycpk/sculkandjaw/blocks/modblocks/TunedSculkJawBlock.java
@@ -3,12 +3,15 @@ package ycpk.sculkandjaw.blocks.modblocks;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -18,8 +21,10 @@ import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.*;
 import org.jspecify.annotations.Nullable;
+import ycpk.sculkandjaw.SculkAndJaw;
 import ycpk.sculkandjaw.blocks.blockentities.TunedSculkJawBlockEntity;
-import ycpk.sculkandjaw.registry.ModTags;
+
+import java.util.Map;
 
 public class TunedSculkJawBlock extends BaseEntityBlock {
     public static final MapCodec<TunedSculkJawBlock> CODEC = simpleCodec(TunedSculkJawBlock::new);
@@ -29,15 +34,12 @@ public class TunedSculkJawBlock extends BaseEntityBlock {
     public static final BooleanProperty STOP_BITE = SculkJawBlock.STOP_BITE;
     public static final VoxelShape COLLISION_SHAPE_OPEN = Shapes.join(
             Block.box(0.0, 0.0, 0.0, 16.0,  16.0, 16.0),
-            Block.box(1.0, 1.0, 1.0, 15.0, 16.0, 15.0),
+            Block.box(1.0, 1.0, 0.0, 15.0, 15.0, 8.0),
             BooleanOp.ONLY_FIRST
     );
-    public static final VoxelShape COLLISION_SHAPE_CLOSE = Shapes.join(
-            Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 16.0),
-            Block.box(1.0, 1.0, 1.0, 15.0, 15.0, 15.0),
-            BooleanOp.ONLY_FIRST
-    );
-    public static final VoxelShape INSIDE_COLLISION_SHAPE = Block.box(1.0, 1.0, 1.0, 15.0, 14.0, 15.0);
+    public static final VoxelShape INSIDE_COLLISION_SHAPE = Block.box(1.0, 1.0, 1.0, 15.0, 15.0, 32.0);
+    public static final Map<Direction, VoxelShape> SHAPES = Shapes.rotateAll(COLLISION_SHAPE_OPEN);
+    public static final Map<Direction, VoxelShape> INSIDE_SHAPES = Shapes.rotateAll(INSIDE_COLLISION_SHAPE);
 
     public TunedSculkJawBlock(Properties properties) {
         super(properties);
@@ -71,18 +73,28 @@ public class TunedSculkJawBlock extends BaseEntityBlock {
     }
 
     @Override
-    protected boolean isPathfindable(BlockState state, PathComputationType type) {
+    protected boolean isPathfindable(BlockState blockState, PathComputationType type) {
         return false;
     }
 
     @Override
+    public BlockState rotate(BlockState blockState, Rotation rotation) {
+        return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState blockState, Mirror mirror) {
+        return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
+    }
+
+    @Override
     protected VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
-        return COLLISION_SHAPE_CLOSE;
+        return (VoxelShape) SHAPES.get(((Direction) blockState.getValue(FACING)));
     }
 
     @Override
     protected VoxelShape getInteractionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
-        return COLLISION_SHAPE_CLOSE;
+        return Shapes.block();
     }
 
     @Override
@@ -92,25 +104,27 @@ public class TunedSculkJawBlock extends BaseEntityBlock {
 
     @Override
     protected VoxelShape getEntityInsideCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Entity entity) {
-        return INSIDE_COLLISION_SHAPE;
+        return (VoxelShape) INSIDE_SHAPES.get(((Direction) blockState.getValue(FACING)));
     }
 
     @Override
     public VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
-        if(collisionContext instanceof EntityCollisionContext) {
-            Entity entity = ((EntityCollisionContext) collisionContext).getEntity();
-            if(entity != null) {
-                if(entity.getType().is(ModTags.IMMUNE_TO_SCULK_JAW)){
-                    return COLLISION_SHAPE_CLOSE;
-                }
-                else if(entity.isShiftKeyDown() && entity.distanceToSqr(blockPos.getCenter().add(0, 0.5, 0)) > 0.2){
-                    return COLLISION_SHAPE_CLOSE;
-                }
-            }
+        return (VoxelShape) SHAPES.get(((Direction) blockState.getValue(FACING)));
+    }
+
+    @Override
+    public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity,
+                             InsideBlockEffectApplier insideBlockEffectApplier, boolean bl) {
+        BlockPos entityPos = entity.blockPosition();
+        SculkAndJaw.LOGGER.info("Tuned Sculk Jaw at pos " + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ()
+        + " detected " + entity.getName().getString() + " at " + entityPos.getX() + ", " + entityPos.getY() + ", " + entityPos.getZ());
+    }
+
+    @Override
+    protected void spawnAfterBreak(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, ItemStack itemStack, boolean bl) {
+        super.spawnAfterBreak(blockState, serverLevel, blockPos, itemStack, bl);
+        if (bl) {
+            this.tryDropExperience(serverLevel, blockPos, itemStack, ConstantInt.of(5));
         }
-        if(blockState.getValue(START_BITE) || blockState.getValue(BITE) || blockState.getValue(STOP_BITE)) {
-            return COLLISION_SHAPE_OPEN;
-        }
-        return COLLISION_SHAPE_CLOSE;
     }
 }
