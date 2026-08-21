3ef704597fbd82704412a5789dd75542b8daa449
Cheesecuter <Cheesecuter777@gmail.com>
2026-08-21 08:01:49 +0800

    Added sOmEtHiNg

--- src/main/java/ycpk/sculkandjaw/registry/ModItems.java
+++ src/main/java/ycpk/sculkandjaw/registry/ModItems.java
@@ -1,11 +1,14 @@
 package ycpk.sculkandjaw.registry;
 
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.component.DeathProtection;
 import ycpk.sculkandjaw.SculkAndJaw;
 import ycpk.sculkandjaw.level.material.ModFluids;
 import ycpk.sculkandjaw.world.food.ModFoods;
@@ -27,7 +30,9 @@ public class ModItems {
             (properties -> {
                 return new BucketItem(ModFluids.SCULK_ACID, properties);
             }),
-            (new Item.Properties()).craftRemainder(Items.BUCKET).stacksTo(1)
+            (new Item.Properties())
+                    .craftRemainder(Items.BUCKET)
+                    .stacksTo(1)
     );
     public static final Item ACIDCOIL_CATTAIL = Items.registerBlock(ModBlocks.ACIDCOIL_CATTAIL);
     public static final Item UMBRAFERN = Items.registerBlock(ModBlocks.UMBRAFERN);
@@ -35,6 +40,16 @@ public class ModItems {
     public static final Item ANTACID_DROPLET = Items.registerItem(
             ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(SculkAndJaw.MOD_ID, "antacid_droplet")),
             Item::new,
-            (new Item.Properties()).food(ModFoods.ANTACID_DROPLET, ModConsumables.ANTACID_DROPLET)
+            (new Item.Properties())
+                    .food(ModFoods.ANTACID_DROPLET, ModConsumables.ANTACID_DROPLET)
+    );
+    public static final Item SCULK_AND_JAW_DEBUG_ITEM = Items.registerItem(
+            ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(SculkAndJaw.MOD_ID, "sculk_and_jaw_debug_item")),
+            Item::new,
+            (new Item.Properties())
+                    .stacksTo(1)
+                    .rarity(Rarity.EPIC)
+                    .component(DataComponents.DEATH_PROTECTION, DeathProtection.TOTEM_OF_UNDYING)
+                    .component(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true)
     );
 }
